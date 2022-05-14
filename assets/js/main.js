2348165f581907fd75c5c4fb69d6d607089f7167
Igor Tozetti <igorct77@gmail.com>
2022-05-14 02:44:24 -0300

    fix some bugs and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,23 @@
-//selecionando o menu
-const menu = document.querySelectorAll(".header-menu li a");
-// função do Smooth scroll
-function smoothScroll(e) {
-  //previnindo padrão dos links
-  e.preventDefault();
-  //selecionando href através do currentTarget
-  const href = e.currentTarget.getAttribute("href");
-  const section = document.querySelector(href);
-  //metódo nativo do js pra ter o efeito de scroll suave.
-  section.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
+function initScrollSuave() {
+  //selecionando o menu
+  const menu = document.querySelectorAll(".header-menu li a");
+  // função do Smooth scroll
+  function smoothScroll(e) {
+    //previnindo padrão dos links
+    e.preventDefault();
+    //selecionando href através do currentTarget
+    const href = e.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    //metódo nativo do js pra ter o efeito de scroll suave.
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+  //adicionando evento de click aos links do menu
+  menu.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
   });
 }
-//adicionando evento de click aos links do menu
-menu.forEach((link) => {
-  link.addEventListener("click", smoothScroll);
-});
+
+initScrollSuave();
